6522721e0981b0a80f256646a9ff97e26f0b04cc
ria4github <ria.guhat@gmail.com>
2023-03-06 13:41:07 +0530

    Initial Code Devotionals

---
+++ nlfyappapi/routes/routes.js
@@ -0,0 +1,135 @@
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = router;
+
+const Model = require("../model/model");
+
+//Post Method
+//router.post("/post", (req, res) => {
+// res.send("Post API");
+//});
+
+//Post Method
+router.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Get all Method
+router.get("/getAll", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by ID Method
+router.get("/getOne/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update by ID Method
+router.patch("/update/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Delete by ID Method
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Post Method - Devotionals
+router.post("/devotionals", async (req, res) => {
+  const data = new Model({
+    subject: req.body.subject,
+    content: req.body.content,
+    datePosted: req.body.datePosted,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Get all Method - Devotionals
+router.get("/devotionals", async (req, res) => {
+  try {
+    const data = await Model.find();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Get by ID Method - Devotional
+router.get("/devotionals/:id", async (req, res) => {
+  try {
+    const data = await Model.findById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Update by ID Method - Devotional
+router.patch("/devotionals/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updatedData = req.body;
+    const options = { new: true };
+
+    const result = await Model.findByIdAndUpdate(id, updatedData, options);
+
+    res.send(result);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Delete by ID Method - Devotional
+router.delete("/devotionals/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted..`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
